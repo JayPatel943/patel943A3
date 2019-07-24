4ceb6400107cfff51d86d01cbb6bc5adefe4f4f2
JayPatel943 <52470290+JayPatel943@users.noreply.github.com>
2019-07-23 21:55:56 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,9 @@
+import {BooksInfo} from '../../app/BooksInfo';
+export const MYBOOKS: BooksInfo[] = 
+[
+    {BookName: 'Wings of fire',BookAuthor: 'Abdul kalam',BookGenres: 'Biography',YearPublished: '1988',image:'Image1.png'},
+    {BookName: 'Experiment With the Truth',BookAuthor: 'Mahatma Gandhi',BookGenres: 'Biography',YearPublished: '2002',image:'Image2.png'},
+    {BookName: 'Jennifer Cruise',BookAuthor: 'Richard bell',BookGenres: 'Biography',YearPublished: '1966',image:'Image3.png'},
+    {BookName: 'The Road Ahead',BookAuthor: 'Bill gates',BookGenres: 'Biography',YearPublished: '1994',image:'Image4.png'}
+
+]
